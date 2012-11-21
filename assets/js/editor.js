cba74c44f808f2857cc3f3d2e1bb2ca2244c56e8
Jürg Lehni <juerg@scratchdisk.com>
2012-11-21 12:33:01 -0800

    Define Http.request() function, to simplify and streamline the creation of Ajax calls.

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -102,6 +102,7 @@ function createPaperScript(element) {
 			scope.remove();
 		scope = new paper.PaperScope();
 		setupConsole();
+		extendScope();
 		// parseInclude() triggers evaluateCode() in the right moment for us.
 		parseInclude();
 	}
@@ -198,6 +199,33 @@ function createPaperScript(element) {
 		scope.console.error('Line ' + lineNumber + ': ' + error);
 		paper.view.draw();
 	};
+
+	function extendScope() {
+		scope.Http = {
+			request: function(options) {
+				var url = options.url,
+					nop = function() {};
+				return $.ajax($.extend({
+					// See if we can use jsonp, by checking the domain for
+					// services known to support it.
+					dataType: /\.json$/.test(url)
+						? /\b(vimeo\.com|twitter\.com|yahoo\.com|googleapis\.com)\b/.test(url)
+							? 'jsonp'
+							: 'json'
+						: (url.match(/\.(xml|html)$/) || [])[1],
+					success: function(data) {
+						(options.onSuccess || nop)(data);
+					},
+					error: function(xhr, error) {
+						(options.onError || nop)(error);
+					},
+					complete: function() {
+						(options.onComplete || nop)();
+					}
+				}, options));
+			}
+		};
+	}
 
 	function parseInclude() {
 		var includes = [];
